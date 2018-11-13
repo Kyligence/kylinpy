35aafcd6b73bafa3d5aeb2f3a24dcf855b0f5e77
Yongjie Zhao <yongjie.zhao@gmail.com>
2018-11-13 13:17:52 +0800

    Rename Kylinpy to KylinClient

--- kylinpy/__init__.py
+++ kylinpy/__init__.py
@@ -4,11 +4,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .kylinpy import Kylinpy, Project
+from .kylinpy import KylinClient, Project
 
 __version__ = '0.0.999dev'
 
 __all__ = [
-    'Kylinpy',
+    'KylinClient',
     'Project',
 ]
