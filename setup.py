3e5e4e6ba27e10176209fe4f6e239f01df128d20
Yongjie Zhao <yongjie.zhao@gmail.com>
2019-02-28 22:28:08 +0800

    Compatible with Python3.7+

--- setup.py
+++ setup.py
@@ -52,9 +52,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
     ],
     entry_points={
         'sqlalchemy.dialects': ['kylin=kylinpy.sqla_dialect:KylinDialect'],
