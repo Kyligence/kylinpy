6e8ec3494d1c80e7bfdf6f827d81d0059f4851f3
Yongjie Zhao <yongjie.zhao@gmail.com>
2020-02-12 12:53:00 +0800

    Fix ut

--- tests/test_kylinpy.py
+++ tests/test_kylinpy.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from kylinpy.kylinpy import dsn_proxy, SERVICES
+from kylinpy.client import Client
 
 
 def test_dsn_proxy():
@@ -22,8 +23,19 @@ def test_dsn_proxy():
     assert cluster.version == 'v1'
     assert cluster.is_pushdown is False
     assert cluster.is_debug is False
-    assert cluster.scheme is 'http'
+    assert cluster.scheme == 'http'
     assert isinstance(cluster.service, SERVICES['v1'])
 
     cluster2 = dsn_proxy('kylin://name@45中文:pwd12@%+@example.com:9000/?version=v2')
     assert isinstance(cluster2.service, SERVICES['v2'])
+
+
+def test_kylinpy_project():
+    cluster = dsn_proxy('kylin://username:password@example')
+    cluster.set_user(username='foo', password='bar')
+    assert cluster.username == 'foo'
+    assert cluster.password == 'bar'
+    assert cluster.basic_auth({}) == {'Authorization': 'Basic Zm9vOmJhcg=='}
+    assert cluster.session_auth({}) == {'Cookie': ''}
+    assert cluster.set_v2_api({}) == {'Accept': 'application/vnd.apache.kylin-v2+json'}
+    assert isinstance(cluster.get_client(), Client)
