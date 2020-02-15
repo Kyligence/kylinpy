fe4df3476332966f59440c1f3ff6b1f253698bb9
Yongjie Zhao <yongjie.zhao@gmail.com>
2020-02-15 12:52:38 +0800

    wip

--- tests/test_cube_source.py
+++ tests/test_cube_source.py
@@ -27,6 +27,38 @@ class TestCubeSource:
         assert cube.identity == '0ef9b7a8-3929-4dff-b59d-2100aadc8dbf'
         assert cube.last_modified == 1560061109000
 
+        assert [d.name for d in cube.dimensions] == [
+            'KYLIN_SALES.TRANS_ID',
+            'KYLIN_CAL_DT.YEAR_BEG_DT',
+            'KYLIN_CAL_DT.MONTH_BEG_DT',
+            'KYLIN_CAL_DT.WEEK_BEG_DT',
+            'KYLIN_CATEGORY_GROUPINGS.USER_DEFINED_FIELD1',
+            'KYLIN_CATEGORY_GROUPINGS.USER_DEFINED_FIELD3',
+            'KYLIN_CATEGORY_GROUPINGS.META_CATEG_NAME',
+            'KYLIN_CATEGORY_GROUPINGS.CATEG_LVL2_NAME',
+            'KYLIN_CATEGORY_GROUPINGS.CATEG_LVL3_NAME',
+            'KYLIN_SALES.LSTG_FORMAT_NAME',
+            'KYLIN_SALES.SELLER_ID',
+            'KYLIN_SALES.BUYER_ID',
+            'BUYER_ACCOUNT.ACCOUNT_BUYER_LEVEL',
+            'SELLER_ACCOUNT.ACCOUNT_SELLER_LEVEL',
+            'BUYER_ACCOUNT.ACCOUNT_COUNTRY',
+            'SELLER_ACCOUNT.ACCOUNT_COUNTRY',
+            'BUYER_COUNTRY.NAME',
+            'SELLER_COUNTRY.NAME',
+            'KYLIN_SALES.OPS_USER_ID',
+            'KYLIN_SALES.OPS_REGION',
+        ]
+
+        assert [m.name for m in cube.measures] == [
+            'GMV_SUM',
+            'BUYER_LEVEL_SUM',
+            'SELLER_LEVEL_SUM',
+            'TRANS_CNT',
+            'SELLER_CNT_HLL',
+            'TOP_SELLER',
+        ]
+
         assert str(cube.from_clause) == (
             '"DEFAULT"."KYLIN_SALES" AS "KYLIN_SALES" '
             'JOIN "DEFAULT"."KYLIN_CAL_DT" AS "KYLIN_CAL_DT" ON "KYLIN_SALES"."PART_DT" = "KYLIN_CAL_DT"."CAL_DT" '  # noqa
