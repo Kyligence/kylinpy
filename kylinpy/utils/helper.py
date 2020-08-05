d69a8d5a7f0b8d5647106428139dcb8d735b6df1
Yongjie Zhao <yongjie.zhao@gmail.com>
2020-08-05 13:17:00 +0800

    output waring logging msg by Logger

--- kylinpy/utils/helper.py
+++ kylinpy/utils/helper.py
@@ -4,11 +4,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import warnings
+from kylinpy.logger import logger
 
 
 def private_v4_api_warnings(fn):
     def wrapper(*args, **kwargs):
-        warnings.warn('Use internal v4-API call, this API-CALL is not in KE4 official manual')
+        logger.warning('Use internal v4-API call, this API-CALL is not in KE4 official manual')
         return fn(*args, **kwargs)
     return wrapper
