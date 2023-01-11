5bdbda9748f094f855400962c2edf666f51df48c
Yongjie Zhao <yongjie.zhao@gmail.com>
2023-01-11 17:26:59 +0800

    chore: release kylinpy 2.8.5
    trigger ci
    
    fix actions

--- kylinpy/__init__.py
+++ kylinpy/__init__.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 
 from .kylinpy import Kylin, create_kylin, KylinCluster, dsn_proxy
 
-__version__ = '0.0.999dev'
+__version__ = '2.8.5'
 
 __all__ = [
     'Kylin', 'create_kylin', 'KylinCluster', 'dsn_proxy',
